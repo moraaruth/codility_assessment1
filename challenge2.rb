9b3ada55253ab5954af0ee8f3e27bf6d97f87bb5
Ruth Moraa <moraar37@gmail.com>
2023-02-24 07:18:24 +0300

    completed challenge2

--- challenge2.rb
+++ challenge2.rb
@@ -1,8 +1,6 @@
 def count_of_characters(input)
     #Create an empty hash to store the character counts
     count = {}
-     # Create an empty array to store the character-count pairs
-    output = []
 
 
      # Loop over each character in the input string
@@ -14,8 +12,15 @@ def count_of_characters(input)
         #add it with a count of 1 and add the character-count pair to the result array
         else
             count[char] = 1
-            output << [char, 1]
         end
     end
-    result
+   #convert the count hash to an array of character-count pairs.
+   # sort by the order of appearance, and return
+   # using to_a to convert to an array
+   # using sort_by to sort by order of appearance
+   count.to_a.sort_by { |pair| input.index(pair[0]) }
 end
+
+p "abracadabra" ---->  [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+p "Code Wars" -----> [['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1]]
+p "233312", ------> [['2', 2], ['3', 3], ['1', 1 ]]
